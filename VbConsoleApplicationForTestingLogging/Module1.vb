9ab54493f3294696440c1337af33b1c9fc2d6424
Patrice Calvé <patware@hotmail.com>
2015-06-10 13:21:28 -0400

    Initial Release to GitHub

---
+++ VbConsoleApplicationForTestingLogging/Module1.vb
@@ -0,0 +1,64 @@
+﻿Module Module1
+
+    Public Enum BarEventIds
+        Starting = 12
+        MessageCritical
+        MessageError
+        MessageInformation
+        MessageResume
+        MessageStart
+        MessageStop
+        MessageSuspend
+        MessageTransfer
+        MessageVerbose
+        MessageWarning
+    End Enum
+
+    Sub Main()
+
+        'This needs to be executed as an Admin
+        If Not System.Diagnostics.EventLog.SourceExists("VbConsoleApplicationForTestingLogging") Then
+            System.Diagnostics.EventLog.CreateEventSource(source:="VbConsoleApplicationForTestingLogging", logName:="MyOwnGroup")
+        End If
+
+        'System.Diagnostics.ConsoleTraceListener
+        'System.Diagnostics.DefaultTraceListener
+        'System.Diagnostics.DelimitedListTraceListener
+        'System.Diagnostics.EventLogTraceListener
+        'System.Diagnostics.EventSchemaTraceListener
+        'System.Diagnostics.TextWriterTraceListener
+        'System.Diagnostics.XmlWriterTraceListener
+
+        System.Diagnostics.Trace.TraceError("TraceError message")
+        System.Diagnostics.Trace.TraceError("TraceError format {0},{1},{2}", 1, 2, 3)
+
+        System.Diagnostics.Trace.TraceInformation("TraceInformation message")
+        System.Diagnostics.Trace.TraceInformation("TraceInformation format {0},{1},{2}", 1, 2, 3)
+
+        System.Diagnostics.Trace.TraceWarning("TraceWarning message")
+        System.Diagnostics.Trace.TraceWarning("TraceWarning format {0},{1},{2}", 1, 2, 3)
+
+        System.Diagnostics.Trace.WriteLine("WriteLine", "Category")
+
+        Dim barTs As New System.Diagnostics.TraceSource("Bar")
+        barTs.TraceEvent(TraceEventType.Information, BarEventIds.Starting)
+        barTs.TraceEvent(TraceEventType.Information, BarEventIds.Starting, "message")
+        barTs.TraceEvent(TraceEventType.Information, BarEventIds.Starting, "Bibi {0}ms", DateTime.Now.Millisecond)
+        barTs.TraceEvent(TraceEventType.Critical, BarEventIds.MessageCritical, "Critical {0}ms", DateTime.Now.Millisecond)
+        barTs.TraceEvent(TraceEventType.Error, BarEventIds.MessageError, "Error {0}ms", DateTime.Now.Millisecond)
+        barTs.TraceEvent(TraceEventType.Information, BarEventIds.MessageInformation, "Information {0}ms", DateTime.Now.Millisecond)
+        barTs.TraceEvent(TraceEventType.Resume, BarEventIds.MessageResume, "Resume {0}ms", DateTime.Now.Millisecond)
+        barTs.TraceEvent(TraceEventType.Start, BarEventIds.MessageStart, "Start {0}ms", DateTime.Now.Millisecond)
+        barTs.TraceEvent(TraceEventType.Stop, BarEventIds.MessageStop, "Stop {0}ms", DateTime.Now.Millisecond)
+        barTs.TraceEvent(TraceEventType.Suspend, BarEventIds.MessageSuspend, "Suspend {0}ms", DateTime.Now.Millisecond)
+        barTs.TraceEvent(TraceEventType.Transfer, BarEventIds.MessageTransfer, "Transfer {0}ms", DateTime.Now.Millisecond)
+        barTs.TraceEvent(TraceEventType.Verbose, BarEventIds.MessageVerbose, "Verbose {0}ms", DateTime.Now.Millisecond)
+        barTs.TraceEvent(TraceEventType.Warning, BarEventIds.MessageWarning, "Warning {0}ms", DateTime.Now.Millisecond)
+
+        '"Default" Trace Source doesn't exist, only "Default" Trace Listener
+        Dim defaultTs As New System.Diagnostics.TraceSource("Default")
+        defaultTs.TraceEvent(TraceEventType.Information, 1234, "It's not there...")
+
+    End Sub
+
+End Module
---
+++ VbConsoleApplicationForTestingLogging/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("VbConsoleApplicationForTestingLogging")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Patware")>
+<Assembly: AssemblyProduct("VbConsoleApplicationForTestingLogging")>
+<Assembly: AssemblyCopyright("Copyright © Patware 2015")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("a4bbaa7e-f542-4af9-b4a6-a65619b40990")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
